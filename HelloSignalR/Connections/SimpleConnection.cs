5a8389bd48677cf3a63b298b7aac5cf2332459e8
Dovydas Navickas <dovydas.nav@gmail.com>
2015-11-21 03:57:43 +0200

    SignalR project with basic connection
    SignalR test client with basic connection. No ASOS stuff yet.

---
+++ HelloSignalR/Connections/SimpleConnection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+
+namespace HelloSignalR.Connections
+{
+    public class SimpleConnection : PersistentConnection
+    {
+        protected override async Task OnConnected(HttpRequest request, string connectionId)
+        {
+            await base.OnConnected(request, connectionId);
+            var identity = request.HttpContext.User.Identity;
+            await Connection.Send(connectionId, $"Connection is {(identity.IsAuthenticated ? "authenticated" : "unauthenticated")}");
+            if (identity.IsAuthenticated)
+            {
+                await Connection.Send(connectionId, $"Authenticated username: ${identity.Name}");
+            }
+        }
+        protected override async Task OnReceived(HttpRequest request, string connectionId, string data)
+        {
+            var identity = request.HttpContext.User.Identity;
+            var authenticatedOrNot = identity.IsAuthenticated ? "authenticated" : "unauthenticated";
+            var clientName = identity.IsAuthenticated ? identity.Name : "client";
+            await Connection.Send(connectionId, $"Received an {authenticatedOrNot} message from {clientName}: {data}");
+        }
+    }
+}
---
+++ HelloSignalR/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNet.Hosting;
+using HelloSignalR.Connections;
+
+namespace HelloSignalR
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication();
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Add the platform handler to the request pipeline.
+            app.UseIISPlatformHandler();
+
+            // Add WebSockets handling for SignalR to support it.
+            app.UseWebSockets();
+
+            app.UseSignalR<SimpleConnection>("/signalr");
+
+            // Configure the HTTP request pipeline.
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
